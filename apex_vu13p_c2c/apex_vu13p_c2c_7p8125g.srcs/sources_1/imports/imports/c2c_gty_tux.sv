bc537babbd60e20943e2b9602237c9d0b349092d
Alexander Madorsky <16050565+madorskya@users.noreply.github.com>
2023-04-20 17:26:07 -0400

    changes for J12/J11 connection restored

--- apex_vu13p_c2c/apex_vu13p_c2c_7p8125g.srcs/sources_1/imports/imports/c2c_gty_tux.sv
+++ apex_vu13p_c2c/apex_vu13p_c2c_7p8125g.srcs/sources_1/imports/imports/c2c_gty_tux.sv
@@ -874,18 +874,18 @@ wire local_do_cc = (cc_cnt == 8'h0);
         .rx_aligned   (ch1_rxbyteisaligned_int),
         
         // for J12-J11
-//        .mgt_tx_data  (hb1_gtwiz_userdata_tx_int),
-//        .mgt_tx_k     (ch1_txctrl2_int)
+        .mgt_tx_data  (hb1_gtwiz_userdata_tx_int),
+        .mgt_tx_k     (ch1_txctrl2_int)
         
         // for J19-J20
-        .mgt_tx_data  (hb0_gtwiz_userdata_tx_int),
-        .mgt_tx_k     (ch0_txctrl2_int)
+//        .mgt_tx_data  (hb0_gtwiz_userdata_tx_int),
+//        .mgt_tx_k     (ch0_txctrl2_int)
     );
     
     assign mgt_rx_data = hb1_gtwiz_userdata_rx_int;
     assign mgt_rx_k    = ch1_rxctrl0_int[3:0];
-    assign mgt_tx_data = hb0_gtwiz_userdata_tx_int;
-    assign mgt_tx_k    = ch0_txctrl2_int[3:0];
+    assign mgt_tx_data = hb1_gtwiz_userdata_tx_int;
+    assign mgt_tx_k    = ch1_txctrl2_int[3:0];
 
     assign c2c_tx_ready      = 1'b1; // always ready
     assign c2c_mmcm_unlocked = 1'b0;
